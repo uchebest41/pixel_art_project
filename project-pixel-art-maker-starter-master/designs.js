84b2e8bc357e19867bd52e4cad0b2d13709ec2d1
uchebest <fairness1346@gmail.com>
2022-05-23 15:11:11 +0100

    pixel art project

---
+++ project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,60 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+const table= document.getElementById('pixelCanvas');
+const gridHeight= document.getElementById('inputHeight');
+const gridWidth= document.getElementById('inputWidth');
+const form = document.getElementById('sizePicker');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  $("#pixelCanvas tr").remove();
+
+  // retrieve the inputHeight and inputWidth
+//   const text = document.getElementById('formInput').value;
+  
+  // create grid
+  // based on inputHeight, create rows in the table
+  // based on inputWidth, create the cells in each row
+  
+  const tableElem = document.getElementById('pixelCanvas');
+  
+  let numberOfRows = gridHeight.value;
+   let numberOfCols = gridWidth.value;
+  for (let i = 0; i < numberOfRows; i++) {
+      let rows= table.insertRow(i);
+      
+      
+      for (let j = 0; j < numberOfCols; j++) {
+       
+        let Cols=rows.insertCell(j);
+        let cell = document.querySelectorAll('td');
+for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener('click', function (event) {
+    
+
+//wanna add color to the background of the cell
+            
+    const colorValue = document.getElementById('colorPicker').value;
+    event.target.style.backgroundColor = colorValue;
+        
+ 
+  });
+
+  }
+}
+
+
+}
+});
+
+
+
+  
+
+
+  
+
+ 
+
+  
