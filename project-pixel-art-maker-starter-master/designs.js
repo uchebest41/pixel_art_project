f4269264fc9a4944deb753d410f442b76ce6cc96
uchebest <fairness1346@gmail.com>
2022-05-24 17:52:36 +0100

    updated

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -2,62 +2,51 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-const table= document.getElementById('pixelCanvas');
-const gridHeight= document.getElementById('inputHeight');
-const gridWidth= document.getElementById('inputWidth');
+const table = document.getElementById('pixelCanvas');
+const gridHeight = document.getElementById('inputHeight');
+const gridWidth = document.getElementById('inputWidth');
 const form = document.getElementById('sizePicker');
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
   // the below code will help to clear the table
-  table.innerHTML="";
+  table.innerHTML = "";
   //  jquery code comment out below
   // $("#pixelCanvas tr").remove();
 
   // retrieve the inputHeight and inputWidth
-//   const text = document.getElementById('formInput').value;
-  
+  //   const text = document.getElementById('formInput').value;
+
   // create grid
   // based on inputHeight, create rows in the table
   // based on inputWidth, create the cells in each row
-  
-  const tableElem = document.getElementById('pixelCanvas');
-  
-  let numberOfRows = gridHeight.value;
-   let numberOfCols = gridWidth.value;
-  for (let i = 0; i < numberOfRows; i++) {
-      let rows= table.insertRow(i);
-      
-      
-      for (let j = 0; j < numberOfCols; j++) {
-       
-        let Cols=rows.insertCell(j);
-        let cell = document.querySelectorAll('td');
-for (let i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', function (event) {
-    
 
-//wanna add color to the background of the cell
-            
-    const colorValue = document.getElementById('colorPicker').value;
-    event.target.style.backgroundColor = colorValue;
-        
- 
-  });
+  const tableElem = document.getElementById('pixelCanvas');
+
+  let numberOfRows = gridHeight.value;
+  let numberOfCols = gridWidth.value;
+  for (let i = 0; i < numberOfRows; i++) {
+    let rows = table.insertRow(i);
+
+
+    for (let j = 0; j < numberOfCols; j++) {
+
+      let Cols = rows.insertCell(j);
+      let cell = document.querySelectorAll('td');
+      for (let i = 0; i < cell.length; i++) {
+        cell[i].addEventListener('click', function (event) {
+
+
+          //wanna add color to the background of the cell
+
+          const colorValue = document.getElementById('colorPicker').value;
+          event.target.style.backgroundColor = colorValue;
+
+
+        });
+
+      }
+    }
+
 
   }
-}
-
-
-}
 });
-
-
-
-  
-
-
-  
-
- 
-
-  
